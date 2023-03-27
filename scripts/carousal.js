29e7930f5e6819bfc7567b564cf4f022fb5bc714
Hossam Gouda <hos.gouda90@gmail.com>
2023-03-27 23:00:44 +0300

    fixed issue for carousel on small devices

--- scripts/carousal.js
+++ scripts/carousal.js
@@ -2,6 +2,7 @@ const images = document.getElementById("images");
 const leftBtn = document.getElementById("left");
 const rightBtn = document.getElementById("right");
 const img = document.querySelectorAll("#images img");
+const carousel = document.querySelector(".carousel");
 
 //iniate index
 let idx = 0;
@@ -10,36 +11,87 @@ let interval = setInterval(run, 2000);
 
 //func changeImage work by checking if the idx bigger
 // ..or less than the images array from the dom
-function changeImage() {
-  if (idx > img.length - 1) {
-    idx = 0;
-  } else if (idx < 0) {
-    idx = img.length - 1;
-  }
-  images.style.transform = `translateX(${-idx * 500}px)`;
+
+
+
+function control() {
+
+    if (window.matchMedia("(max-width: 562px)").matches) {
+
+        for (var i = 1; i < img.length; i++) {
+
+            img[i].classList.add(".img-sm");
+            img[i].setAttribute("id", "box");
+            img[i].style.width = `${carousel.getBoundingClientRect().width}px`;
+        }
+
+        function changeImage() {
+            if (idx > img.length - 1) {
+                idx = 0;
+            } else if (idx < 0) {
+                idx = img.length - 1;
+            }
+            images.style.transform = `translateX(${
+              -idx * carousel.getBoundingClientRect().width
+            }px)`;
+        }
+        changeImage();
+    } else {
+        for (var i = 1; i < img.length; i++) {
+            img[i].classList.remove(".img-sm");
+            img[i].removeAttribute('id');
+            img[i].style.width = `${carousel.getBoundingClientRect().width}px`
+
+        }
+
+        function changeImage() {
+            if (idx > img.length - 1) {
+                idx = 0;
+            } else if (idx < 0) {
+                idx = img.length - 1;
+            }
+            images.style.transform = `translateX(${
+              -idx * carousel.getBoundingClientRect().width
+            }px)`;
+        }
+        changeImage()
+    }
+
 }
 
+// function changeImage() {
+//     if (idx > img.length - 1) {
+//         idx = 0;
+//     } else if (idx < 0) {
+//         idx = img.length - 1;
+//     }
+//     images.style.transform = `translateX(${-idx * 500}px)`;
+// }
+
 //func run increment the indx, call changeImage func
+
+
+
 function run() {
-  idx++;
-  changeImage();
+    idx++;
+    control();
 }
 
 //to avoid issues with auto sliding
 function resetInterval() {
-  clearInterval(interval);
-  interval = setInterval(run, 2000);
+    clearInterval(interval);
+    interval = setInterval(run, 2000);
 }
 
 rightBtn.addEventListener("click", () => {
-  idx++;
-  changeImage();
-  resetInterval();
+    idx++;
+    control();
+    resetInterval();
 });
 
 leftBtn.addEventListener("click", () => {
-  idx--;
-  changeImage();
-  resetInterval();
+    idx--;
+    control();
+    resetInterval();
 });
 // images Silder end
